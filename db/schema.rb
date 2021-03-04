9178df2064d70cdaa99261d39048a4a761d0a003
“dwbra” <dworkmang@gmail.com>
2021-03-04 13:04:50 +1100

    working on payment integration redirect. Need to fix webhooks using ultrahook

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_110016) do
+ActiveRecord::Schema.define(version: 2021_03_04_013105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_110016) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sold"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
