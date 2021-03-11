be8b2e5b2b442781de8a609d25220c8cb86acfa9
“dwbra” <dworkmang@gmail.com>
2021-03-11 15:50:26 +1100

    added validation to prices and added puts to stripe to try and see error on production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,7 @@
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+puts "api_key from credentials:"
+p Rails.application.credentials.dig(:stripe, :secret_key)
 Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+puts "api_key on Stripe module:"
+p Stripe.api_key
