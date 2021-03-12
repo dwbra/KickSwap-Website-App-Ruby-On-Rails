e6b1e84a61b00fa3ee274adbda1924792778c573
“dwbra” <dworkmang@gmail.com>
2021-03-12 17:19:35 +1100

    updated styling on a first of the primary pages

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,1 @@
-# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
-
-puts "api_key from credentials:"
-p Rails.application.credentials.dig(:stripe, :secret_key)
 Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
-puts "api_key on Stripe module:"
-p Stripe.api_key
