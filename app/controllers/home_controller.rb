8c5abf03bff6659d84553dfeb943cc517e36d67b
“dwbra” <dworkmang@gmail.com>
2021-03-14 11:30:46 +1100

    working on css. about to implement mini magick

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def page
     @listings = Listing.all
+    @render_carousel = true
   end
 
   def account
     @listings = Listing.all
     @my_purchases = Listing.where(buyer_id: current_user.id)
   end
-end
+end 
