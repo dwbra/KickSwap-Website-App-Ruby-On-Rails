9ef19b4ccbf86dc77bfd675346504813aec506e9
“dwbra” <dworkmang@gmail.com>
2021-03-18 12:08:00 +1100

    stripe updates

--- app/javascript/packs/stripe-button.js
+++ app/javascript/packs/stripe-button.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("[data-stripe='payment']");
     if (button) {
         button.addEventListener("click", () => {
-        const {sessionId, publicKey} = button
+        const {sessionId, publicKey} = button.dataset
           const stripe = Stripe(
             publicKey
           )
